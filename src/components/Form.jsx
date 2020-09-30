775aeb1b559af7981916176c99f823b55cb53ee0
Derek Lam <dereklam67@gmail.com>
2020-09-30 13:56:51 -0700

    email regex fix

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -25,7 +25,7 @@ export default () => {
     const validate = (e) => {
         e.preventDefault();
         let { name: { value: name }, email: { value: email }, message: { value: message }} = formData;
-        
+
         let formDataWithErr = {
             name: {
                 value: name,
@@ -58,7 +58,7 @@ export default () => {
             };
             valid = false;
         }
-        else if (/[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/.test(formData.email)) {
+        else if (!/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(formData.email.value)) {
             formDataWithErr.email = {
                 value: formDataWithErr.email.value,
                 error: 'Invalid email format'
@@ -124,8 +124,7 @@ export default () => {
                 cols="30" 
                 rows="10"
                 value={formData.message.value}
-                onChange={handleChange} 
-                placeholder="Enter your message" />
+                onChange={handleChange} />
                 {
                     formData.message.error && (
                         <p className="error app-contact__error-text">{formData.message.error}</p>
