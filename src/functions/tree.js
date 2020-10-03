de297f0ebeecc9a65fe86180527f0b39bd619685
unknown <maxwellwong91@gmail.com>
2020-10-02 20:18:43 -0700

    added traversal methods and replace node

--- src/functions/tree.js
+++ src/functions/tree.js
@@ -1,11 +1,17 @@
+import { v4 as uuidv4 } from 'uuid';
 export class Node {
-    constructor(value) {
+    constructor(value, uuid) {
         this.value = value;
         this.left = null;
         this.right = null;
+        this.uuid = uuid || uuidv4();
     }
 }
 
+/**
+ * Converts string representation of binary tree to node
+ * @param s - string representation of binary tree
+ */
 export const parseTree = (s) => {
     if (s === null) {
         return null;
@@ -38,6 +44,10 @@ export const printTree = (node) => {
     }
 }
 
+/**
+ * Converts root node of tree to string representation
+ * @param node - root node of tree structure
+ */
 export const nodeToString = (node) => {
     if (node === null) {
         return ("null");
@@ -53,3 +63,150 @@ export const nodeToString = (node) => {
 
     return s;
 }
+
+/**
+ * Creates new node to replace node
+ * @param node - root node of tree structure
+ * @param value - value to replace node value with
+ * @param uuid - uuid of node we want to update value for
+ */
+export const replaceNodeValue = (node, value, uuid) => {
+    if (node === null) {
+        return;
+    }
+
+    // Create new node to replace left
+    if (node.left !== null && node.left.uuid === uuid) {
+        let nextNode = new Node(value, uuid);
+        let prevNode = node.left;
+        node.left = nextNode;
+        nextNode.left = prevNode === null ? null : prevNode.left;
+        nextNode.right = prevNode === null ? null : prevNode.right;
+        return node.left;
+    }
+
+    // Create new node to replace right
+    if (node.right !== null && node.right.uuid === uuid) {
+        let nextNode = new Node(value, uuid);
+        let prevNode = node.right;
+        node.right = nextNode;
+        nextNode.left = prevNode === null ? null : prevNode.left;
+        nextNode.right = prevNode === null ? null : prevNode.right;
+        return node.right;
+    }
+
+    replaceNodeValue(node.left, value, uuid);
+    replaceNodeValue(node.right, value, uuid);
+
+    return node.right;
+}
+
+/**
+ * Updates the id of all nodes in subtree
+ * @param node - root node of the tree structure
+ * @param start = id to set the node to
+ */
+export const updateId = (node, start) => {
+    if (node === null) {
+        return start;
+    }
+
+    node.uuid = start;
+    start = updateId(node.left, start + 1);
+    start = updateId(node.right, start + 1);
+
+    return start + 1;
+}
+
+/**
+ * Returns the inorder traversal of a binary tree
+ * @param node - root node of the tree structure
+ * @param l - list that holds inorder traversal of binary tree
+ */
+export const inOrderTraversal = (node) => {
+    function helper(node, l) {
+        if (node === null) {
+            return;
+        }
+    
+        helper(node.left, l);
+    
+        l.push(node.value);
+        helper(node.right, l);
+    }
+    var res = []
+    helper(node, res);
+    return JSON.stringify(res);
+}
+
+/**
+ * Returns the preorder traversal of a binary tree
+ * @param node - root node of the tree structure
+ * @param l - list that holds preorder traversal of binary tree
+ */
+export const preOrderTraversal = (node, l) => {
+    function helper(node, l) {
+        if (node === null) {
+            return;
+        }
+    
+        l.push(node.value);
+        helper(node.left, l);
+        helper(node.right, l);
+    }
+    
+    var res = []
+    helper(node, res);
+    return JSON.stringify(res);
+}
+
+
+/**
+ * Returns the postorder traversal of a binary tree
+ * @param node - root node of the tree structure
+ * @param l - list that holds postorder traversal of binary tree
+ */
+export const postOrderTraversal = (node, l) => {
+    function helper(node, l) {
+        if (node === null) {
+            return;
+        }
+    
+        helper(node.left, l);
+        helper(node.right, l);
+        l.push(node.value);
+    }
+
+    var res = []
+    helper(node, res);
+    return JSON.stringify(res);
+    
+}
+
+
+/**
+ * Returns the level order traversal of a binary tree
+ * @param node - root node of the tree structure
+ */
+export const levelOrderTraversal = (node) => {
+    if (node === "null") {
+        return [];
+    }
+    var q = [];
+    var ans = [];
+    q.push(node);
+    while (q.length > 0) {
+        var first = q.shift();
+        ans.push(first.value);
+        if (first.left !== null) {
+            q.push(first.left);
+        }
+
+        if (first.right !== null) {
+            q.push(first.right);
+        }
+    }
+
+    return JSON.stringify(ans);
+}
+
--- src/test/tree.test.js
+++ src/test/tree.test.js
@@ -1,4 +1,4 @@
-import {nodeToString, parseTree} from "../functions/tree.js";
+import {nodeToString, parseTree, replaceNodeValue, updateId, inOrderTraversal, preOrderTraversal, postOrderTraversal, levelOrderTraversal} from "../functions/tree.js";
 import {Node} from "../functions/tree.js"
 
 describe("Test parseTree method", () => {
@@ -26,7 +26,8 @@ describe("Test parseTree method", () => {
         right.right = new Node(12);
 
         var result = parseTree(input);
-
+        updateId(result, 0);
+        updateId(expected, 0);
         expect(result).toMatchObject(expected);
 
         var input = JSON.parse("[0, null, [5, [1, null, null], null]]");
@@ -36,7 +37,10 @@ describe("Test parseTree method", () => {
         expected.right.left = new Node(1);
 
         var result = parseTree(input);
+        updateId(result, 0);
+        updateId(expected, 0);
         expect(result).toMatchObject(expected);
+
 
         var input = JSON.parse("[-1, [-2, [2, null, null], null], [0, null, [1, null, [-3, null, null]]]]");
 
@@ -48,9 +52,14 @@ describe("Test parseTree method", () => {
         expected.right.right.right = new Node(-3);
 
         var result = parseTree(input);
+        updateId(result, 0);
+        updateId(expected, 0);
         expect(result).toMatchObject(expected);
     })
+})
 
+
+describe ("Test nodeToString method", () => {
     it ("Should give string when correct input", () => {
         var input = new Node(1);
         input.left = new Node(5);
@@ -64,7 +73,6 @@ describe("Test parseTree method", () => {
         var expected = "[1, [5, null, null], [4, [7, [4, null, null], [8, null, null]], [12, null, null]]]";
 
         var result = nodeToString(input);
-        console.log(result);
         expect(result).toBe(expected);
 
         var input = new Node(0);
@@ -86,5 +94,176 @@ describe("Test parseTree method", () => {
         var result = nodeToString(input);
         expect(result).toBe(expected);
     })
+})
 
+describe("Test replace node value", () => {
+    it ("Should create new node to replace existing node", () => {
+        var input = new Node(0, 0);
+        var root = new Node(1, 123);
+        input.right = root;
+
+        var expected = new Node(5, 123);
+
+        var result = replaceNodeValue(input, 5, 123);
+        
+        expect(result).toMatchObject(expected);
+
+        var input = new Node(0, 0);
+        var root = new Node(1, 123);
+        input.right = root;
+        root.right = new Node(4, 111);
+        root.right.left = new Node(6, 964);
+        root.right.right = new Node(7, 432);
+
+        var expected = new Node(5, 123);
+        expected.right = new Node(4, 111);
+        expected.right.left = new Node(6, 964);
+        expected.right.right = new Node(7, 432);
+
+        var result = replaceNodeValue(input, 5, 123);
+        
+        expect(result).toMatchObject(expected);
+
+        var input = new Node(0, 0);
+        var root = new Node(-1, 123);
+        input.right = root;
+        root.left = new Node(-2, 111);
+        root.left.left = new Node(2, 964);
+        root.right = new Node(0, 432);
+        root.right.right = new Node(-3, 231);
+        root.right.right.right = new Node(-3, 777);
+
+        var expected = new Node(-1, 123);
+        expected.left = new Node(-2, 111);
+        expected.left.left = new Node(2, 964);
+        expected.right = new Node(0, 432);
+        expected.right.right = new Node(-3, 231);
+        expected.right.right.right = new Node(-2, 777);
+
+        var result = replaceNodeValue(input, -2, 777);
+
+        expect(result).toMatchObject(expected);
+
+        var input = new Node(0, 0);
+        var root = new Node(-1, 123);
+        input.right = root;
+        root.left = new Node(-2, 111);
+        root.left.left = new Node(2, 964);
+        root.right = new Node(0, 432);
+        root.right.right = new Node(-3, 231);
+        root.right.right.right = new Node(-3, 777);
+
+        var expected = new Node(-1, 123);
+        expected.left = new Node(-2, 111);
+        expected.left.left = new Node(0, 964);
+        expected.right = new Node(0, 432);
+        expected.right.right = new Node(-3, 231);
+        expected.right.right.right = new Node(-3, 777);
+
+        var result = replaceNodeValue(input, 0, 964);
+
+        expect(result).toMatchObject(expected);
+
+    })
+})
+
+describe ("Test inorder traversal", () => {
+    it ("Should give proper inorder traversal", () => {
+        var input = new Node(1);
+        input.right = new Node(2);
+        input.right.left = new Node(3);
+
+        var expected = "[1,3,2]"
+        var result = inOrderTraversal(input);
+        expect(result).toBe(expected);
+
+        var input = new Node(1);
+        input.left = new Node(9);
+        input.left.left = new Node(3);
+        input.left.left.left = new Node(1);
+        input.left.right = new Node(6);
+        input.right = new Node(2);
+        input.right.right = new Node(3);
+
+
+        var expected = "[1,3,9,6,1,2,3]"
+        var result = inOrderTraversal(input);
+        expect(result).toBe(expected);
+
+    })
+})
+
+describe ("Test preorder traversal", () => {
+    it ("Should give proper preorder traversal", () => {
+        var input = new Node(1);
+        input.right = new Node(2);
+        input.right.left = new Node(3);
+
+        var expected = "[1,2,3]"
+        var result = preOrderTraversal(input);
+        expect(result).toBe(expected);
+
+        var input = new Node(1);
+        input.left = new Node(9);
+        input.left.left = new Node(3);
+        input.left.left.left = new Node(1);
+        input.left.right = new Node(6);
+        input.right = new Node(2);
+        input.right.right = new Node(3);
+
+
+        var expected = "[1,9,3,1,6,2,3]"
+        var result = preOrderTraversal(input);
+        expect(result).toBe(expected);
+    })
+})
+
+describe ("Test postorder traversal", () => {
+    it ("Should give proper postorder traversal", () => {
+        var input = new Node(1);
+        input.right = new Node(2);
+        input.right.left = new Node(3);
+
+        var expected = "[3,2,1]"
+        var result = postOrderTraversal(input);
+        expect(result).toBe(expected);
+
+        var input = new Node(1);
+        input.left = new Node(9);
+        input.left.left = new Node(3);
+        input.left.left.left = new Node(1);
+        input.left.right = new Node(6);
+        input.right = new Node(2);
+        input.right.right = new Node(3);
+
+
+        var expected = "[1,3,6,9,3,2,1]"
+        var result = postOrderTraversal(input);
+        expect(result).toBe(expected);
+    })
+})
+
+describe ("Test level order traversal", () => {
+    it ("Should give proper level order traversal", () => {
+        var input = new Node(1);
+        input.right = new Node(2);
+        input.right.left = new Node(3);
+
+        var expected = "[1,2,3]"
+        var result = levelOrderTraversal(input);
+        expect(result).toBe(expected);
+
+        var input = new Node(1);
+        input.left = new Node(9);
+        input.left.left = new Node(3);
+        input.left.left.left = new Node(1);
+        input.left.right = new Node(6);
+        input.right = new Node(2);
+        input.right.right = new Node(3);
+
+
+        var expected = "[1,9,2,3,6,3,1]"
+        var result = levelOrderTraversal(input);
+        expect(result).toBe(expected);
+    })
 })
