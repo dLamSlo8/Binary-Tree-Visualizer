c8d384a1d77496cffd281908ba271ba4b0a143d2
unknown <maxwellwong91@gmail.com>
2020-11-09 23:14:17 -0800

    added deletetree function not tested yet

--- src/functions/algorithms/bst.js
+++ src/functions/algorithms/bst.js
@@ -21,12 +21,13 @@ export class Node {
 /**
  * Returns array of steps that occur from inserting a node into the tree
  * as well as the new tree
- * @param value - value of node to insert
  * @param node - root node of the tree
+ * @param value - value of node to insert
  * @param uuid - uuid of node to create
  */
-export const insertNode = (value, node, uuid = null) => {
-    function helper(value, node, uuid, moves) {
+export const insertNode = (node, value, uuid = null) => {
+    // need to discuss how to handle root node
+    function helper(node, value, uuid, moves) {
         if (node == null) {
             var newNode = new Node(value, uuid);
             moves.push(newNode.uuid);
@@ -34,9 +35,10 @@ export const insertNode = (value, node, uuid = null) => {
         }
         moves.push(node.uuid);
         if (value <= node.value) {
-            node.left = helper(value, node.left, uuid, moves);
-        } else {
-            node.right = helper(value, node.right, uuid, moves)
+            node.left = helper(node.left, value, uuid, moves);
+        } 
+        else {
+            node.right = helper(node.right, value, uuid, moves)
         }
 
         return node;
@@ -45,7 +47,92 @@ export const insertNode = (value, node, uuid = null) => {
     var rootCopy = new Node(0, 1, node);
     var moves = [];
 
-    helper(value, rootCopy, uuid, moves);
+    helper(rootCopy, value, uuid, moves);
+    return [moves, rootCopy];
+}
+
+/**
+ * Returns array of steps that occur from deleting a node in the tree
+ * as well as the new tree
+ * @param node - root node of the tree
+ * @param value - value of node to delete
+ */
+export const deleteNode = (node, value) => {
+    function helper(node, value, rootCopy, moves) {
+        if (node == null) {
+            // treat as error if it doesn't exist for now, might change need to discuss
+            throw "A node with this value does not exist in the tree";
+        }
+        moves.push(node.uuid);
+
+        if (value === node.value) {
+            if (node.left == null && node.right == null) {
+                return null;
+            }
+            else if (node.left == null) {
+                return node.right;
+            }
+            else if (node.right == null) {
+                return node.left;
+            }
+            else {
+                return inorderSuccessor(rootCopy, node);
+            }
+        }
+        else if (value < node.value) {
+            let left = helper(node.left, value, rootCopy, moves);
+            node.left = left ? left : node.left;
+        }
+        else {
+            let right = helper(node.right, value, rootCopy, moves);
+            node.right = right ? right : node.right;
+        }
+
+        
+    }
+
+    /**
+     * Returns node of inorder successor of node and removes that node from
+     * tree
+     * @param node - root node of the tree structure
+     * @param nodeForSuccessor - node that we want to find succesor for
+     */
+    function inorderSuccessor(node, nodeForSuccessor) {
+        function findMin(node, parent) {
+            if (node.left == null) {
+                if (parent) {
+                    parent.right = null;
+                }
+                return node;
+            }
+
+            var left = findMin(node.left, null);
+
+            if (left) {
+                node.left = null;
+                return left;
+            }
+        }
+
+        if (nodeForSuccessor.right == null) {
+            let curr = node.left;
+
+            // keep going through right subtree to get largest possible value
+            while (curr && curr.value < nodeForSuccessor.value) {
+                curr = curr.right;
+            }
+            return curr;
+
+        }
+
+        let successor = findMin(nodeForSuccessor.right, nodeForSuccessor);
+        return successor;
+    }
+
+    var rootCopy = new Node(0, 1, node);
+    var moves = [];
+
+    helper(rootCopy, uuid, rootCopy, moves);
     return [moves, rootCopy];
 }
 
--- src/test/algorithms/bst.test.js
+++ src/test/algorithms/bst.test.js
@@ -2,7 +2,7 @@ import { insertNode } from "../../functions/algorithms/bst.js";
 import { Node } from "../../functions/algorithms/bst.js";
 
 describe("Test insertNode function", () => {
-    it ("Should give proper moves array", () => {
+    it ("Should give proper moves array and tree", () => {
         var input = new Node(5, 1);
         input.left = new Node(5, 2);
         input.right = new Node(7, 3);
@@ -14,7 +14,7 @@ describe("Test insertNode function", () => {
 
         var expectedMoves = [1, 2, 4]
 
-        var result = insertNode(5, input, 4);
+        var result = insertNode(input, 5, 4);
         
         expect(result[0]).toEqual(expectedMoves);
         expect(result[1]).toMatchObject(expectedTree);
@@ -32,7 +32,7 @@ describe("Test insertNode function", () => {
 
         var expectedMoves = [1, 2, 4, 5]
 
-        var result = insertNode(5, input, 5);
+        var result = insertNode(input, 5, 5);
         expect(result[0]).toEqual(expectedMoves);
         expect(result[1]).toMatchObject(expectedTree);
 
@@ -51,7 +51,7 @@ describe("Test insertNode function", () => {
 
         var expectedMoves = [1, 3, 5, 6]
 
-        var result = insertNode(9, input, 6);
+        var result = insertNode(input, 9, 6);
 
         expect(result[0]).toEqual(expectedMoves);
         expect(result[1]).toMatchObject(expectedTree);
@@ -71,7 +71,87 @@ describe("Test insertNode function", () => {
 
         var expectedMoves = [1, 3, 6]
 
-        var result = insertNode(6, input, 6);
+        var result = insertNode(input, 6, 6);
+
+        expect(result[0]).toEqual(expectedMoves);
+        expect(result[1]).toMatchObject(expectedTree);
+
+    })
+
+    
+})
+
+describe("Test deleteNode function", () => {
+    it ("Should give proper moves array and tree", () => {
+        var input = new Node(5, 1);
+        input.left = new Node(5, 2);
+        input.right = new Node(7, 3);
+
+        var expectedTree = new Node(5, 1);
+        expectedTree.left = new Node(5, 2);
+        expectedTree.left.left = new Node(5, 4);
+        expectedTree.right = new Node(7, 3);
+
+        var expectedMoves = [1, 2, 4]
+
+        var result = insertNode(input, 5, 4);
+        
+        expect(result[0]).toEqual(expectedMoves);
+        expect(result[1]).toMatchObject(expectedTree);
+
+        var input = new Node(5, 1);
+        input.left = new Node(5, 2);
+        input.left.left = new Node(4, 4);
+        input.right = new Node(7, 3);
+
+        var expectedTree = new Node(5, 1);
+        expectedTree.left = new Node(5, 2);
+        expectedTree.left.left = new Node(4, 4);
+        expectedTree.left.left.right = new Node(5, 5);
+        expectedTree.right = new Node(7, 3);
+
+        var expectedMoves = [1, 2, 4, 5]
+
+        var result = insertNode(input, 5, 5);
+        expect(result[0]).toEqual(expectedMoves);
+        expect(result[1]).toMatchObject(expectedTree);
+
+        var input = new Node(5, 1);
+        input.left = new Node(5, 2);
+        input.left.left = new Node(4, 4);
+        input.right = new Node(7, 3);
+        input.right.right = new Node(10, 5);
+
+        var expectedTree = new Node(5, 1);
+        expectedTree.left = new Node(5, 2);
+        expectedTree.left.left = new Node(4, 4);
+        expectedTree.right = new Node(7, 3);
+        expectedTree.right.right = new Node(10, 5);
+        expectedTree.right.right.left = new Node(9, 6);
+
+        var expectedMoves = [1, 3, 5, 6]
+
+        var result = insertNode(input, 9, 6);
+
+        expect(result[0]).toEqual(expectedMoves);
+        expect(result[1]).toMatchObject(expectedTree);
+
+        var input = new Node(5, 1);
+        input.left = new Node(5, 2);
+        input.left.left = new Node(4, 4);
+        input.right = new Node(7, 3);
+        input.right.right = new Node(10, 5);
+
+        var expectedTree = new Node(5, 1);
+        expectedTree.left = new Node(5, 2);
+        expectedTree.left.left = new Node(4, 4);
+        expectedTree.right = new Node(7, 3);
+        expectedTree.right.left = new Node(6, 6);
+        expectedTree.right.right = new Node(10, 5);
+
+        var expectedMoves = [1, 3, 6]
+
+        var result = insertNode(input, 6, 6);
 
         expect(result[0]).toEqual(expectedMoves);
         expect(result[1]).toMatchObject(expectedTree);
